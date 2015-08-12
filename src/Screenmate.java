6eba3c25daa72269389f3369421875365405b7ba
tlhoward <t.howard@utexas.edu>
2015-08-11 23:12:08 -0500

    Initial commit; very bare

---
+++ src/Screenmate.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.ImageIcon;
+import javax.swing.Timer;
+
+/*
+ * This program (should) load all images of Guilmon from
+ * Season 3 of Digimon. Functionality of the program will be
+ * updated as I draw out the images, but in this first version
+ * Guilmon will appear on the bottom right corner of your
+ * desktop. 
+ * 
+ * Right now he only stands and sits. I need to figure out how
+ * to draw his walk cycle. Right click Guilmon to end program.
+ */
+
+public class Screenmate extends javax.swing.JWindow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
