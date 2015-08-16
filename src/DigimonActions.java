07c28c6b0e3b84a3f9dfc242bd645ba5e4917114
tlhoward <t.howard@utexas.edu>
2015-08-16 03:13:35 -0500

    Digimon teleports along x axis every 3 seconds. No walk animation yet

--- src/DigimonActions.java
+++ src/DigimonActions.java
@@ -1,5 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.ImageIcon;
 import javax.swing.Timer;
@@ -14,17 +15,30 @@ public class DigimonActions {
 	
 	protected void idleWalk() {
 
-		ActionListener taskPerformer = new ActionListener() {
+		delay = 3000; // 3000 milliseconds, 3 seconds
+
+		timer = new Timer(delay, new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
-				int y = digimon.returnCurrentY();
-				digimon.setNewLocation(20, y);
+				idleWalkHelper();
 			}
-		};
-
+		});
 		
-		int delay = 5000;
-		new Timer(delay, taskPerformer).start();
+		timer.setRepeats(true);
+		timer.start();
 
+	}
+	
+	protected void idleWalkHelper() {
+		// change delay to random numbers so digimon wont walk exact same length
+		// every time
+		// also switch directions every so often. have that be random too
+		// walk different distances as well. set x as random number, keep y same
+		
+		Random rand = new Random();
+		
+		int x = rand.nextInt(1600);
+		int y = digimon.returnCurrentY();
+		digimon.setNewLocation(x, y);
 	}
 	
 	protected void idleStand(ImageIcon image) {
@@ -36,4 +50,7 @@ public class DigimonActions {
 		// I need to figure it out and add it later
 		digimon.setDigimonImage(image);
 	}
+	
+	private int delay;
+	private Timer timer;
 }
