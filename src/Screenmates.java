45073cb01c4b7707348278dc0355860eddb86e71
tlhoward <t.howard@utexas.edu>
2018-04-18 16:52:14 -0500

    added system tray icon; placeholder context menu

--- src/Screenmates.java
+++ src/Screenmates.java
@@ -39,6 +39,7 @@ public class Screenmates extends JWindow {
 				stats = new Status();
 				stats.setNewState();
 				guilmon.setVisible(true);
+				createAndShowSystemTrayIcon();
 				start();
 			}
 		});
@@ -62,4 +63,52 @@ public class Screenmates extends JWindow {
     	SavedState state;
     	InputStream file;
     }
+    
+    private static void createAndShowSystemTrayIcon() {
+        //Check the SystemTray is supported
+        if (!SystemTray.isSupported()) {
+            System.out.println("SystemTray is not supported");
+            return;
+        }
+
+        Image image = Toolkit.getDefaultToolkit().getImage("images/egg.png");
+        final PopupMenu popup = new PopupMenu();
+        final TrayIcon trayIcon = new TrayIcon(image, "Screenmate", popup);
+        final SystemTray tray = SystemTray.getSystemTray();
+        trayIcon.setImageAutoSize(true);
+
+       
+        // Create a pop-up menu components
+        MenuItem aboutItem = new MenuItem("About");
+        CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
+        CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
+        Menu displayMenu = new Menu("Display");
+        MenuItem errorItem = new MenuItem("Error");
+        MenuItem warningItem = new MenuItem("Warning");
+        MenuItem infoItem = new MenuItem("Info");
+        MenuItem noneItem = new MenuItem("None");
+        MenuItem exitItem = new MenuItem("Exit");
+       
+        //Add components to pop-up menu
+        popup.add(aboutItem);
+        popup.addSeparator();
+        popup.add(cb1);
+        popup.add(cb2);
+        popup.addSeparator();
+        popup.add(displayMenu);
+        displayMenu.add(errorItem);
+        displayMenu.add(warningItem);
+        displayMenu.add(infoItem);
+        displayMenu.add(noneItem);
+        popup.add(exitItem);
+       
+        trayIcon.setPopupMenu(popup);
+       
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            System.out.println("TrayIcon could not be added.");
+        }
+    }
+    
 }
